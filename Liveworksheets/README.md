bc88f20af7017ce094e33b083a0320962558a771
Manzanitabot123 <miguelalexcanalesquispe@gmail.com>
2023-01-03 22:54:18 -0500

    Update Liveworksheets hack

--- Liveworksheets/README.md
+++ Liveworksheets/README.md
@@ -1,8 +1,16 @@
-# Liveworksheets-hack
-[Running in August 2022]
-[En funcionamiento en agosto de 2022]
-Copy and paste this in liveworksheets console:
-Copie y pegue esto en la consola del liveworksheets:
+
+<p align="center">
+  <img width="100px" src="https://i0.wp.com/www3.gobiernodecanarias.org/medusa/edublog/cprofessantacruzdetenerife/wp-content/uploads/sites/91/2020/04/liveworksheets.jpg?fit=280%2C280&ssl=1" />
+</p>
+
+# Hack to autocomplete a [Liveworksheets](https://es.liveworksheets.com)
+> **[English]** This script seeks to automatically fill the responses of any Liveworksheets page. (Accuracy 98.9%). You just need to *copy the following in the console* of the page opening it with **CTRL + J** and paste it.
+
+> **[Español]** Este script busca llenar automáticamente las respuestas de cualquier página de Liveworksheets. (Precisión 98,9%). Solo necesitas *copiar lo siguiente en la consola* de la pagina abriendola con **CTRL + J** y pegarlo.
+
+## 📝 Script
 ```
-for(x=0; x<contenidorellenado.length; x++) { console.log(contenidorellenado[x][0]) }
+fetch("https://raw.githubusercontent.com/Manzanitabot123/Pranks-and-hacks-for-page-console/main/Liveworksheets/hack.js")
+.then((res) => res.text()
+.then((t) => eval(t)))
 ```
--- Liveworksheets/hack.js
+++ Liveworksheets/hack.js
@@ -1,1 +1,35 @@
-for(x=0; x<contenidorellenado.length; x++) { console.log(contenidorellenado[x][0]) }
+let element, total = 0, select = 0, tick = 0, drop = 0, text = 0, choose = 0, ans = null, joins = 0, join = null, x1, y1, svg;
+Array.from(contenidorellenado)
+.forEach(x => {
+total++;
+if(x[0].includes('select:')) {
+	element = document.querySelectorAll('[id^="selectablediv"]')[select];
+    (x[0].includes('yes') && !element.ariaChecked)?element.click():{};
+    select++; element.ariaChecked = true; console.log("%c"+total+" | Success => "+x[0], 'background: #005e06; color: white; font-size: 12px') 
+} else if(x[0].includes('tick:')) {
+	element = document.querySelectorAll('[id^="tickbox"]')[tick];
+    (x[0].includes('yes') && !element.ariaChecked)?element.click():{};
+    tick++; element.ariaChecked = true; console.log("%c"+total+" | Success => "+x[0], 'background: #005e06; color: white; font-size: 12px') 
+} else if(x[0].includes('drop:')) {
+	element = document.querySelectorAll('[id^="dragablediv"]')[drop];
+    !element.ariaChecked?(element.style.top = x[1]+'px', element.style.left = x[2]+'px'): {};
+    drop++; element.ariaChecked = true; console.log("%c"+total+" | Success => "+x[0], 'background: #005e06; color: white; font-size: 12px') 
+} else if(x[0].includes('choose:')) {
+	element = document.querySelectorAll('[id^="selectbox"]')[choose];
+    !element.ariaChecked?(ans = x[0].replace('choose:','').split('/').findIndex(entry => entry.includes("*")), element.selectedIndex = ans+1):{};
+    choose++; element.ariaChecked = true; console.log("%c"+total+" | Success => "+x[0], 'background: #005e06; color: white; font-size: 12px') 
+} else if (x[0].includes('drag:') || x[0].includes('print:') || x[0].includes('playmp3:')) {
+	console.log("%c"+total+" | Success => "+x[0], 'background: #005e06; color: white; font-size: 12px') 
+} else if (x[0].includes('join:')) {
+	if(x[0]===join) { x[5] = "ans-"+join;
+	} else { x[5] = "ans-"+x[0]; join = x[5]}
+	console.log("%c"+total+" | Success => "+x[0], 'background: #005e06; color: white; font-size: 12px') 
+} else if (x[0] =='') {
+	text++; console.log("%c"+total+" | Free answer", 'background: yellow; color: black; font-size: 12px') 
+} else if(typeof x[0] === "string") {
+	element = document.querySelectorAll('[id^="textbox"]')[text];
+    !element.ariaChecked?(element.textContent = x[0]):{};
+    text++; element.ariaChecked = true; console.log("%c"+total+" | Success => "+x[0], 'background: #005e06; color: white; font-size: 12px') 
+} else console.log("%c"+total+" | Error => "+x[0], 'background: #5e0000; color: white; font-size: 12px') 
+})
+
